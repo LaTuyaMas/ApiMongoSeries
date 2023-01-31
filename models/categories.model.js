81e5d18656a97dbf9a10724973e633514ffb9040
LaTuyaMas <miguel.cano.roman@cieep.com>
2023-01-31 12:53:18 +0100

    después de frontend

--- models/categories.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-const categorieSchema = new Schema({
-    name: {type: String, required: true},
-    icon: {type: String, required: true}
-});
-
-module.exports = mongoose.model('Categorie', categorieSchema, 'categories');
